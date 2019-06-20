d90527f04e32d7131efead9958fd5608a98d24cc
Nikos Katsikanis <nkatsikanis@gmail.com>
2019-06-20 12:28:47 +0100

    add logistic regression, remove visualiser

--- src/Playground.ts
+++ src/Playground.ts
@@ -1,11 +1,10 @@
-import { Visualiser } from './Visualiser'
-
 var tf = require('@tensorflow/tfjs')
+console.log(tf.getBackend())
+console.log('what backend')
 console.log(tf.getBackend())
 
 export class Playground {
   private context2D: CanvasRenderingContext2D
-  private visualiser: Visualiser = new Visualiser(document.querySelector('#chartContainer'))
   constructor() {
     this.setupListeners()
   }
@@ -22,8 +21,45 @@ export class Playground {
     document.querySelector('#tf_model2').addEventListener('click', () => {
       this.tf_model2()
     })
-    document.querySelector('#sigmoid').addEventListener('click', () => {
-      this.visualiser.visualiseSigmoid()
+    document.querySelector('#train').addEventListener('click', () => {
+      this.train()
+    })
+  }
+  train() {
+    // Fit a quadratic function by learning the coefficients a, b, c.
+    const xs = tf.tensor1d([0, 1, 2, 3])
+    const ys = tf.tensor1d([1.1, 5.9, 16.8, 33.9])
+
+    const a = tf.scalar(Math.random()).variable()
+    const b = tf.scalar(Math.random()).variable()
+    const c = tf.scalar(Math.random()).variable()
+
+    // y = a * x^2 + b * x + c.
+    const f = x =>
+      a
+        .mul(x.square())
+        .add(b.mul(x))
+        .add(c)
+    const loss = (pred, label) => {
+      return pred
+        .sub(label)
+        .square()
+        .mean()
+    }
+
+    const learningRate = 0.01
+    const optimizer = tf.train.sgd(learningRate)
+
+    // Train the model.
+    for (let i = 0; i < 10; i++) {
+      optimizer.minimize(() => loss(f(xs), ys))
+    }
+
+    // Make predictions.
+    console.log(`a: ${a.dataSync()}, b: ${b.dataSync()}, c: ${c.dataSync()}`)
+    const preds = f(xs).dataSync()
+    preds.forEach((pred, i) => {
+      console.log(`x: ${i}, pred: ${pred}`)
     })
   }
   seq_modal() {
